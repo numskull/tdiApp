602682387fa5a6e50aaa2745373fcde23eb447b3
numskull <numskulldesign@gmail.com>
2019-02-20 16:17:01 -0500

    removed migrations folder

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class developmentConfig(Config):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     BASE_DIR = basedir
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://mapyqqxxdlebuf:b0e059c94bedf2d345829ab4ef236d43d3641efb6da961e25a927561b54092c0@ec2-54-83-44-4.compute-1.amazonaws.com:5432/d1hk326n4v5lm9herok') or 'sqlite:///' \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' \
                               + os.path.join(basedir, 'data-test.sqlite')
 
 config = {
